1f83cd0baedf019144fb55ecb1c36f0bc05a7745
chingTheCoder <chingliu10@outlook.com>
2023-07-04 08:19:04 +0300

    order creation lets see its reaction

--- routes/menu/menu.js
+++ routes/menu/menu.js
@@ -35,16 +35,41 @@ router.post('/addtocart', async (req, res) => {
     res.json({ 
                 response : true , 
                 phoneid : user.props.phoneID,
-                product : productToAdd 
+                product : productToAdd ,
+                totalProducts : arrayOfProduct
             })
 
 })
 
 router.post('/sendmessage', async (req, res) => {
 
-        let { mobilePhone, mobileId } = req.body
+    let { mobilePhone, mobileId, products } = req.body
+
+    //change the state of the user
+    await users.set(mobilePhone, {
+        state : 'orderCreated'
+    })
+
+    let generatedMessage = 'Your Order includes\n' 
+    let totalPrice = 0
+    let two = "\u{0032}\u{FE0F}\u{20E3}"
+    let one = "\u{0031}\u{FE0F}\u{20E3}"
+    let three = "\u{0033}\u{FE0F}\u{20E3}"
+
+    for (let x = 0 ; x < products.length; x++) {
+
+        let string = `${x + 1}. ${products[x].productName} @ ${products[x].productPrice}\n\n`
+        generatedMessage = generatedMessage + string
+        totalPrice = totalPrice + Number(products[x].productPrice)
+
+    }
+
+    generatedMessage = generatedMessage + `*Total Price* : ${totalPrice}\n\n
+        type corresponding Number to proceed\n\n${one}. To *SEND* Order\n${two}. To REMOVE an Item from order\n${three}. To CANCEL order
+
+    `
     
-     
+    //generate message 
     let facebookRequest = await fetch(`https://graph.facebook.com/v12.0/${mobileId}/messages?access_token=${process.env.WHATSAPP_TOKEN}`, {
             method : 'post',
             headers : {"content-type" : "application/json"},
@@ -56,6 +81,17 @@ router.post('/sendmessage', async (req, res) => {
         })
     let facebookResponse = await facebookRequest.json()
     console.log(facebookResponse)
+    let facebookRequest2 =  await fetch(`https://graph.facebook.com/v12.0/${mobileId}/messages?access_token=${process.env.WHATSAPP_TOKEN}`, {
+        method : 'post',
+        headers : {"content-type" : "application/json"},
+        body : JSON.stringify({
+            messaging_product: "whatsapp",
+            to: mobilePhone,
+            text: { body : generatedMessage}
+        })
+    })
+    let lastRequest = await facebookRequest2.json()
+    console.log(lastRequest)
     res.json({ message : 'success'})
 
 
