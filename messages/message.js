0003e8b5f14620b19dbae07fe72c3c9a9be50dc9
chingTheCoder <chingliu10@outlook.com>
2023-07-05 07:38:35 +0300

    lets see this order details

--- messages/message.js
+++ messages/message.js
@@ -38,44 +38,98 @@ async function message (userInput, username, userId, phoneId) {
 }
 
 
-async function checkState (userInput, user, userId) {
+async function checkState (userInput, user, userId, phoneId) {
 
+    //the big if
+    if (user.props.state == 'nostate') {
     //send user to delivery
-    if(user.props.state == 'nostate' && userInput == '1') {
-        
-        //set order choice to pickup
-        await users.set(userId , {
-                state : 'order',
-                orderChoice : 'pickup'
-        })
+                if(userInput == '1') {
+                    
+                    //set order choice to pickup
+                    await users.set(userId , {
+                            state : 'order',
+                            orderChoice : 'pickup'
+                    })
 
-        return   {
-            messaging_product : "whatsapp",
-            to : userId,
-            text : { body : `Tap link to see our menu${'\u261F'}\n\n https://courageous-pear-snapper.cyclic.app/menu/${userId}`}
-        }
+                    return   {
+                        messaging_product : "whatsapp",
+                        to : userId,
+                        text : { body : `Tap link to see our menu${'\u261F'}\n\n https://courageous-pear-snapper.cyclic.app/menu/${userId}`}
+                    }
+
+                }
+
+                //send user to pickup
+                if(userInput == '2') {
+                    
+                    await users.set(userId , {
+                        state : 'order',
+                        orderChoice : 'delivery'
+                    })
+                    return   {
+                        messaging_product : "whatsapp",
+                        to : userId,
+                        text : { body : `Tap link to see our menu${'\u261F'}\n\n https://courageous-pear-snapper.cyclic.app/menu/${userId}`}
+                    }
+
+                }
+
+                
+
+                return {
+                    messaging_product : "whatsapp",
+                    to : userId,
+                    text : { body : `Type 1 for delivery and 2 for pickup`}
+                }
 
     }
 
-    //send user to pickup
-    if(user.props.state == 'nostate' && userInput == '2') {
-        
-        await users.set(userId , {
-            state : 'order',
-            orderChoice : 'delivery'
-        })
-        return   {
-            messaging_product : "whatsapp",
-            to : userId,
-            text : { body : `Tap link to see our menu${'\u261F'}\n\n https://courageous-pear-snapper.cyclic.app/menu/${userId}`}
-        }
+    //when order has being created
+    if (user.props.state == 'orderCreated') {
 
-    }
+                if(userInput == '1') {
+                    //save order to the database
+                    return {
+                        messaging_product : "whatsapp",
+                        to : userId,
+                        text : { body : `Your Order has being received you will contacted within 2 hours ${"\uD83D\uDE03"}`}
 
-    return {
-        messaging_product : "whatsapp",
-        to : userId,
-        text : { body : `type 1 for delivery and 2 for pickup`}
+                    } 
+                }
+
+                if(userInput == '2') {
+
+                    await users.set(userId, {
+                        state : 'removingItem'
+                    })
+
+                    return {
+                        messaging_product : "whatspp",
+                        to : userId,
+                        text : { body : "Below is the list of items to remove type a corresponding number to remove"}
+                    }
+
+                }
+
+                if(userInput == '3') {
+
+                    await users.set(userId, {
+                        state : 'orderCanceled'
+                    })
+
+                    return {
+                        messaging_product : "whatsapp",
+                        to : userId,
+                        text : { body : `Your Order has being canceled ${"\uD83D\UDE26"}`}
+                    }
+                }
+                
+                return {
+                    messaging_product : "whatsapp", 
+                    to : userId,
+                    text : { body : "Type 1 to send order 2 to remove items and 3 to cancel order"}
+                }
+
     }
     
 }
